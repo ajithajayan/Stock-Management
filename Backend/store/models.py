3cde694a7286eab8e29a90595364e442fd21b2c3
Ajith Ajayan <ajithajayan@gmail.com>
2024-08-09 15:56:50 +0530

    product code + barcode + branch creation model updation

--- Backend/store/models.py
+++ Backend/store/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
+import os
+import barcode
+from barcode.writer import ImageWriter
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.utils.crypto import get_random_string
 
 # Supplier model
 class Supplier(models.Model):
@@ -30,15 +36,46 @@ class Brand(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     purchase_date = models.DateField()
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='products')
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE, related_name='products')
     unit = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE, related_name='products')
     expiry_date = models.DateField()
     opening_stock = models.PositiveIntegerField(validators=[MinValueValidator(0)])
     manufacturing_date = models.DateField()
-    product_code = models.CharField(max_length=100, unique=True)
+    product_code = models.CharField(max_length=100, unique=True, blank=True)
     barcode = models.CharField(max_length=100, unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Automatically generate the product code if not provided
+        if not self.product_code:
+            last_product = Product.objects.order_by('-id').first()
+            if last_product:
+                last_code = int(last_product.product_code[1:])
+                new_code = last_code + 1
+            else:
+                new_code = 5001
+            self.product_code = f'P{new_code}'
+
+        # Automatically generate the barcode if not provided
+        if not self.barcode:
+            # Generate a random string of 12 digits for the barcode
+            barcode_number = get_random_string(length=12, allowed_chars='0123456789')
+            ean = barcode.get('ean13', barcode_number, writer=ImageWriter())
+            
+            # Create the directory if it does not exist
+            barcode_dir = os.path.join('media', 'barcodes')
+            if not os.path.exists(barcode_dir):
+                os.makedirs(barcode_dir)
+
+            # Save the barcode image
+            barcode_path = os.path.join(barcode_dir, self.product_code)
+            ean.save(barcode_path)
+
+            # Save the barcode number in the database
+            self.barcode = barcode_number
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -47,8 +84,20 @@ class Product(models.Model):
 class Branch(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    branch_code = models.AutoField(primary_key=True)
+    branch_code = models.CharField(max_length=10, primary_key=True, unique=True, blank=True)
     contact_details = models.CharField(max_length=15)
+
+    def save(self, *args, **kwargs):
+        if not self.branch_code:
+            last_branch = Branch.objects.order_by('-branch_code').first()
+            if last_branch and last_branch.branch_code.startswith("BR"):
+                last_code = int(last_branch.branch_code[2:])
+                new_code = last_code + 1
+            else:
+                new_code = 121211
+            self.branch_code = f'BR{new_code}'
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
